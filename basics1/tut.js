78af13005d437861f20af9494c54aa5e263436ca
barsha20061001 <barshadgp212@gmail.com>
2025-07-07 17:34:30 +0000

    datatypes

---
+++ basics1/tut.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version 
+//alert(3+3)// we are using nodejs , not broswser
+
+//code readibility is important//
+console.log("barsha")
+let name ="barsha"
+let age = 12
+let isLoggedIn = false
+let state;
+//number => 2 to power 53 
+//bigint
+// string=>""
+//boolean ==> true/false
+//null==> standalone value
+// undefined =>
+    //symbol ==> unique
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
---
+++ basics1/variable.js
@@ -0,0 +1,16 @@
+const accountId = 234567
+let accountEmail = "barsha123@gmail.com"
+var accountPassword = "12345"
+accountcity = "Surat"
+let accountState;
+
+// accountId = "2" // not allowed
+accountEmail ="mayukh23@gmail.com"
+accountPassword = "mayukh"
+accountcity = "Bengaluru"
+/* prefer not to use var because of issue in block scope and functional scope */
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountcity , accountState])
+
+
+
