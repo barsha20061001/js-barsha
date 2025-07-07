9a8e62e68ad3566d91706c7ef841708f554e7f25
barsha20061001 <barshadgp212@gmail.com>
2025-07-07 19:50:06 +0000

    more

--- basics1/conversion.js
+++ basics1/conversion.js
@@ -1,22 +1,49 @@
-let score = "Barsha"
+//let score = "Barsha"
 
 //console.log(typeof score);
 //console.log(typeof(score));
-let valueInNumber = Number(score)
+//let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
 //console.log( valueInNumber);
 
 //"33"=> 33
 // "33abc" => NaN
 // true => 1; false => 0
-let isLoggedIn= "Barsha"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+//let isLoggedIn= "Barsha"
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn);
 
 //1 ==> true; 0==> false
 // "" ==> false
 // "Barsha" ==> true
-let someNumber = 33
-let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+//let someNumber = 33
+//let StringNumber = String(someNumber)
+//console.log(StringNumber);
+//console.log(typeof StringNumber);
+// ***********operations*****************
+//let value =3
+//let negValue = -value;
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+let str1="Hello"
+let str2="Barsha"
+let str3= str1 + str2
+//console.log(str3);
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log((3+4) *5 % 3);
+//console.log(+true);
+//console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
