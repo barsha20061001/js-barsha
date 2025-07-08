4e03e25ec5be876f1924c3925c55ef683c445370
barsha20061001 <barshadgp212@gmail.com>
2025-07-08 19:22:35 +0000

    strings

---
+++ basics1/strings.js
@@ -0,0 +1,27 @@
+const name="Barsha"
+const repoCount=50
+//console.log(name + repoCount +" Value ");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName= new String("rekha")
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+//console.log(gameName.lenght);
+//console.log(gameName.toUpperCase());
+console.log(gameName.charAt('2'));
+console.log(gameName.indexOf('t'));
+const newString = gameName.substring(0,4)
+console.log(newString);
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "barsha"
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url ="barshadgp@gmail.com"
+console.log(url.replace('%20',"_"))
+console.log(url.includes('sundar'))
+console.log(gameName.split('-'));
+
+
+
