b88a2491b543885b17c6dfff16b6edca8b0ee928
barsha20061001 <barshadgp212@gmail.com>
2025-07-12 14:47:09 +0000

    control

---
+++ basics1/control.js
@@ -0,0 +1,68 @@
+//if
+// const isUserLOggedin = true
+// const temperature= 41
+// if(temperature == 40){
+    // console.log("less than 50");
+// }else{
+// console.log("temperature is greater than 50");
+// }
+// console.log("execute")
+// 
+//  const score= 200
+// if (score>100){
+    // let power= "fly"
+    // console.log(`user power : ${power}`)
+// }
+//  console.log(`user power : ${power}`)
+// 
+
+// const balance = 1000
+// if (balance<500){
+    // console.log("less than 500");
+// }
+// else if (balance<750){
+    // console.log("less than 750");
+// }else if (balance<750){
+    // console.log("less than 750");
+// }
+// else{
+    // console.log("less than 1200");
+// }
+
+// let num = prompt("enter a number:");
+// if (num%5===0){
+    // console.log(num,"is a multiple of 5");
+// }
+// else{
+    // console.log(num,"is NOT a multiple of 5");
+// }
+
+// const readline = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// 
+// readline.question("Enter a number: ", (num) => {
+//   num = Number(num);
+//   if (num % 5 === 0) {
+    // console.log(num, "is a multiple of 5");
+//   } else {
+    // console.log(num, "is NOT a multiple of 5");
+//   }
+//   readline.close();
+// });
+
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+// if (loggedInFromGoogle || loggedInFromEmail) {
+    // console.log("User logged in");
+// }
